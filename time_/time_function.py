c0a4eaf170d1b419e4b93dedacbddcf20f836827
Mikhail <91376833+Mikhail-Kushnerev@users.noreply.github.com>
2022-04-15 23:57:00 +0300

    Module for measuring the time of operation

---
+++ time_/time_function.py
@@ -0,0 +1,24 @@
+import time
+
+
+def time_of_function(func):
+    def wrapper(*args):
+        start_time = time.time()
+        print(f'Запуск функции: {func.__name__}')
+        result = func(*args)
+        execution_time = round(time.time() - start_time, 2)
+        print(f'Время выполнения функции: {execution_time} c.')
+        return result
+    return wrapper
+
+
+# @time_of_function
+# def summa(a, b):
+#     return a + b
+
+
+# # c = time_of_function(summa)
+# print(summa(2, 4))
+
+# if __name__ == '__main__':
+#     time_of_function()
