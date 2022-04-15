883d8fe2cca9e8930f7984d9be2d77779cbd7732
Mikhail-Kushnerev <mikushnerev@yandex.ru>
2022-04-16 00:15:11 +0300

    gitignore

--- time_/time_function.py
+++ time_/time_function.py
@@ -2,10 +2,10 @@ import time
 
 
 def time_of_function(func):
-    def wrapper(*args):
+    def wrapper(*kwargs):
         start_time = time.time()
         print(f'Запуск функции: {func.__name__}')
-        result = func(*args)
+        result = func(*kwargs)
         execution_time = round(time.time() - start_time, 2)
         print(f'Время выполнения функции: {execution_time} c.')
         return result
